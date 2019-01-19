41caed108708e92e17499e88239adedaaaad62b8
Wendi Chen <epanistamai.proles@gmail.com>
2019-01-19 20:53:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,84 @@
 class Song
-  attr_accessor :name, :artist_name
   @@all = []
+  attr_accessor :artist_name, :name
 
+
+  def initialize
+   
+    
+  end
+
+  def self.create
+    @song = self.new
+    @@all << @song
+    @song
+  end
+  
   def self.all
     @@all
   end
-
-  def save
-    self.class.all << self
+ 
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
   end
+  
+ 
+  
+  def self.create_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    @@all << song
+    song
+  end
+  
+  def self.find_by_name(song_name)
+    var = nil
+    @@all.each do |song|
+      if song.name == song_name
+        var = song
+             end
+      
+    end
+    var
+  end
+  
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+     
+  
+end
+  
 
+  def self.create_from_filename(new_song)
+      song_array = new_song.split("-"|| ".")
+      
+      song = self.new
+      song.name = song_array[1]
+      @artist_name = song_array[0]
+      
+  
+  end
+  
+  def self.alphabetical
+    @@all.sort.collect
+  end
+  
+  def self.new_from_filename(song_name)
+    title = song_name.split(" - " || ".mp3")
+    self.new_by_name(title[1])
+    @artist_name = title[0]
+    
+  end  
+  
+  def self.create_from_filename(song_name)
+    
+  end
+  
+   def self.destroy_all
+    @@all.clear
+  end
+  
+  
 end
